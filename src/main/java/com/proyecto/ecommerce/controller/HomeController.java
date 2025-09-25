e42915d9d1ca67a76cf1eca9f03fefe569187f2b
Gianfranco Salazar <gsalazare92@gmail.com>
2025-09-24 22:51:19 -0500

    Obtener la cantidad y buscar el producto en el HomeController para añadir al carrito

--- src/main/java/com/proyecto/ecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.proyecto.ecommerce.controller;
 
+import com.proyecto.ecommerce.model.DetalleOrden;
+import com.proyecto.ecommerce.model.Orden;
 import com.proyecto.ecommerce.model.Producto;
 import com.proyecto.ecommerce.service.ProductoService;
 import org.slf4j.Logger;
@@ -7,11 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -24,6 +25,15 @@ public class HomeController {
 
     @Autowired
     private ProductoService productoService;
+
+    // Variable para almacenar los detalles de la orden
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    // Datos de la orden
+    Orden orden = new Orden();
+
+
+
 
     @GetMapping("")
     public String home(Model model) {
@@ -44,7 +54,16 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad){
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = productoService.get(id);
+        log.info("Producto agregado: {}", optionalProducto.get());
+        log.info("Cantidad: {}", cantidad);
+
+
         return "usuario/carrito";
     }
 
