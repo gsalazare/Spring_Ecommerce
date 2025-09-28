3945018c1f44d3d4c67472569a14b150a02c64e1
Gianfranco Salazar <gsalazare92@gmail.com>
2025-09-28 12:22:06 -0500

    Obtener usuario login y sesion

--- src/main/java/com/proyecto/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/proyecto/ecommerce/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.proyecto.ecommerce.controller;
 
 import com.proyecto.ecommerce.model.Usuario;
 import com.proyecto.ecommerce.service.IUsuarioService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuario")
@@ -30,6 +33,33 @@ public class UsuarioController {
         logger.info("Usuario {}", usuario);
         usuario.setTipo("USER");
         usuarioService.save(usuario);
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "usuario/login";
+    }
+
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session){
+        logger.info("Accesos {}", usuario);
+
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+        logger.info("Usuario de base de datos {}", user.get());
+
+        if (user.isPresent()){
+            session.setAttribute("idusuario", user.get().getId());
+            if (user.get().getTipo().equals("ADMIN")){
+                return "redirect:/administrador";
+            }else {
+                return "redirect:/";
+            }
+        }
+            else{
+                logger.info("No existe el usuario");
+            }
+
         return "redirect:/";
     }
 
