e13efe99c081101d49a21dbcfc569a3be9fc9504
Gianfranco Salazar <gsalazare92@gmail.com>
2025-09-27 19:48:37 -0500

    Metodo para generar el numero de la orden

--- src/main/java/com/proyecto/ecommerce/service/IOrdenService.java
+++ src/main/java/com/proyecto/ecommerce/service/IOrdenService.java
@@ -2,6 +2,9 @@ package com.proyecto.ecommerce.service;
 
 import com.proyecto.ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save(Orden orden);
 }
--- src/main/java/com/proyecto/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/proyecto/ecommerce/service/OrdenServiceImpl.java
@@ -5,14 +5,70 @@ import com.proyecto.ecommerce.repository.IOrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements IOrdenService {
 
     @Autowired
     private IOrdenRepository ordenRepository;
 
+
     @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
     }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generarNumeroOrden(){
+        int numero = 0;
+        String numeroConcatenado = "";
+
+        List<Orden> ordenes = findAll();
+
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if(ordenes.isEmpty()){
+            numero = 1;
+        }else {
+            numero = numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+        if(numero < 10){
+            numeroConcatenado = "000000000" + String.valueOf(numero);
+        }else if(numero < 100) {
+            numeroConcatenado = "00000000" + String.valueOf(numero);
+        }
+        else if(numero < 1000) {
+            numeroConcatenado = "0000000" + String.valueOf(numero);
+        }
+        else if(numero < 10000) {
+            numeroConcatenado = "000000" + String.valueOf(numero);
+        }
+        else if(numero < 100000) {
+            numeroConcatenado = "00000" + String.valueOf(numero);
+        }
+        else if(numero < 1000000) {
+            numeroConcatenado = "0000" + String.valueOf(numero);
+        }
+        else if(numero < 10000000) {
+            numeroConcatenado = "000" + String.valueOf(numero);
+        }
+        else if(numero < 100000000) {
+            numeroConcatenado = "00" + String.valueOf(numero);
+        }
+        else if(numero < 1000000000) {
+            numeroConcatenado = "0" + String.valueOf(numero);
+        }
+
+        return numeroConcatenado;
+    }
+
 }
