16cb98854659517a5beb5c33dff6aec7fd040621
Gianfranco Salazar <gsalazare92@gmail.com>
2025-10-05 01:47:11 -0500

    Funcionalidad para ver las ordenes por el admin

--- src/main/java/com/proyecto/ecommerce/controller/AdministradoController.java
+++ src/main/java/com/proyecto/ecommerce/controller/AdministradoController.java
@@ -2,6 +2,7 @@ package com.proyecto.ecommerce.controller;
 
 
 import com.proyecto.ecommerce.model.Producto;
+import com.proyecto.ecommerce.service.IOrdenService;
 import com.proyecto.ecommerce.service.IUsuarioService;
 import com.proyecto.ecommerce.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,10 @@ public class AdministradoController {
     @Autowired
     private IUsuarioService usuarioService;
 
+    @Autowired
+    private IOrdenService ordenService;
+
+
     @GetMapping("")
     public String home(Model model) {
 
@@ -35,6 +40,12 @@ public class AdministradoController {
     public String usuarios(Model model){
         model.addAttribute("usuarios", usuarioService.findAll());
         return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordenes")
+    public String ordenes(Model model){
+        model.addAttribute("ordenes", ordenService.findAll());
+        return "administrador/ordenes";
     }
 
 }
